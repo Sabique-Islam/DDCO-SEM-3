3507e6040e2bf8b5588ed4b2f549fb6722cb4553
sabique <sabiqueislam7@gmail.com>
2025-07-15 16:29:37 +0530

    fix: some changes lol

--- verilog/runv.sh
+++ verilog/runv.sh
@@ -1,5 +1,7 @@
-#!cmd/ctrl + shift + B 
+#! cmd/ctrl + shift + B 
 
 iverilog -o output filename.v && vvp output
 
 #! clear && iverilog -o output filename.v && vvp output
+
+#! iverilog -o half_adder_tb.vvp half_adder_tb.v
