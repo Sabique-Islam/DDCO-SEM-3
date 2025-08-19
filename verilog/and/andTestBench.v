edd8562875b457c1325c548b1e00412cfdd96721
sabique <sabiqueislam7@gmail.com>
2025-08-19 08:59:55 +0530

    add andGate

---
+++ verilog/and/andTestBench.v
@@ -0,0 +1,15 @@
+`include "and.v"
+
+module and_test;
+    reg a,b;
+    wire y;
+    and_test(a,b,y);
+    initial begin
+        $monitor("a=%b, b=%b, y=%b", a, b, y);
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $finish;
+    end
+endmodule
